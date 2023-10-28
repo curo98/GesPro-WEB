8ab5793b491d996828f0973f258bafd8024f576e
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-10-28 13:09:21 -0500

    Config API POLICIES accept

--- app/Http/Controllers/Api/SupplierRequestController.php
+++ app/Http/Controllers/Api/SupplierRequestController.php
@@ -189,12 +189,8 @@ class SupplierRequestController extends Controller
         $selectedPolicies = $request->input('selectedPolicies');
 
         foreach ($selectedPolicies as $policyData) {
-        // Asocia cada política a la solicitud de proveedor con el ID y el estado desde el array
-            DB::table('supplier_requests_policies')->insert([
-                'id_supplier_request' => $id_supplier_request,
-                'id_policie' => $policyData['id'],
-                'accepted' => $policyData['isChecked'], // Establece el estado según el array
-            ]);
+            // Asocia cada política a la solicitud de proveedor con el ID de la solicitud y el estado utilizando Eloquent
+            $supplierRequest->policies()->attach($policyData['id'], ['accepted' => $policyData['isChecked']]);
         }
         return response()->json(['message' => 'Registro exitoso como proveedor'], 201);
     }
