02698765a29d17e8eed2bf81d6e549300e19ffd5
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-03 14:05:36 -0500

    Update profile and clear cache of user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\JwtAuth;
+
 
 class UserController extends Controller
 {
