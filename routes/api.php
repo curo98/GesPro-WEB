dac581db7ad39eaa8cccce94c7436b347363cb68
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-03 11:50:26 -0500

    Edit profile

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Auth\AuthController;
 
 /*
