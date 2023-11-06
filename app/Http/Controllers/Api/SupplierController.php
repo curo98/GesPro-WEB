e7f647d04c79f53b70ac3a6c2f1d792a3db47109
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-06 13:19:53 -0500

    Edit supplier test.

--- app/Http/Controllers/Api/SupplierController.php
+++ app/Http/Controllers/Api/SupplierController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use \App\Models\Supplier;
 
 class SupplierController extends Controller
