1253c3f45bd692151d0adebf350c0868482e301b
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-06 16:21:11 -0500

    Create supplier.

--- routes/api.php
+++ routes/api.php
@@ -59,7 +59,7 @@ Route::middleware('auth:api')->group(function () {
 
     /*RUTAS PARA EL CRUD DE SOLICITUDES DE PROVEEDORES*/
     Route::get('/suppliers', [App\Http\Controllers\Api\SupplierController::class, 'index']);
-    Route::get('/supplier/store', [App\Http\Controllers\Api\SupplierController::class, 'store']);
+    Route::post('/supplier/store', [App\Http\Controllers\Api\SupplierController::class, 'store']);
     Route::get('/supplier/{id}', [App\Http\Controllers\Api\SupplierController::class, 'show']);
     Route::get('/supplier/{id}/edit', [App\Http\Controllers\Api\SupplierController::class, 'edit']);
     Route::post('/supplier/{id}/update', [App\Http\Controllers\Api\SupplierController::class, 'update']);
