27aad572c3ad3027fbfdc514f9583995eb067c1f
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-11 02:14:20 -0500

    Crud users - add..

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Support\Facades\Auth;
 
