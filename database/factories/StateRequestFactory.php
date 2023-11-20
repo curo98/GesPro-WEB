bd06219cbf726d8f15d5e8797622600b79fb2eb0
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-20 16:17:13 -0500

    requests lastTransitions....hym.o

--- database/factories/StateRequestFactory.php
+++ database/factories/StateRequestFactory.php
@@ -17,7 +17,7 @@ class StateRequestFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->unique()->randomElement(['Enviado', 'Por recibir', 'Recibida', 'Por validar', 'Validada', 'Por aprobar', 'Aprobada', 'En correccion', 'Corregida', 'Rechazada', 'Cancelada']),
+            'name' => $this->faker->unique()->randomElement(['Enviado', 'Por recibir', 'Recibida', 'Por validar', 'Validada', 'Por aprobar', 'Aprobada', 'Rechazada', 'Cancelada']),
             'description' => $this->faker->sentence(),
         ];
     }
