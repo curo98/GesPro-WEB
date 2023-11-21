87f53914e510fd11167b8ac98120af612a1f29e9
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-20 23:04:45 -0500

    test lists..requests..l..limn

--- app/Http/Controllers/Api/SupplierRequestController.php
+++ app/Http/Controllers/Api/SupplierRequestController.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Auth;
 
 class SupplierRequestController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         // return "LLegue";
@@ -110,29 +108,11 @@ class SupplierRequestController extends Controller
                 'questions'
             )->get();
 
-            // Obtener el estado "Por aprobar"
-            $estadoPorAprobar = DB::table('state_requests')
-                ->where('name', 'Por aprobar')
-                ->first();
-            $stateToApprove = $estadoPorAprobar->id;
-
-            // Obtener el estado "Aprobada"
-            $estadoAprobada = DB::table('state_requests')
-                ->where('name', 'Aprobada')
-                ->first();
-            $stateApproved = $estadoAprobada->id;
-
-            // Obtener el estado "Validada"
-            $estadoValidada = DB::table('state_requests')
-                ->where('name', 'Validada')
-                ->first();
-            $stateValidated = $estadoValidada->id;
-
-            // Obtener el estado "Recibida"
-            $estadoRecibida = DB::table('state_requests')
-                ->where('name', 'Recibida')
-                ->first();
-            $stateReceived = $estadoRecibida->id;
+            // Definir estados
+            $stateToApprove = getStateId('Por aprobar');
+            $stateApproved = getStateId('Aprobada');
+            $stateValidated = getStateId('Validada');
+            $stateReceived = getStateId('Recibida');
 
             // Filtrar las solicitudes de proveedores con las transiciones específicas
             $supplierRequestsWithTransitions = $supplierRequests->filter(function ($supplierRequest) use ($stateToApprove, $stateApproved, $stateValidated, $stateReceived) {
@@ -207,6 +187,10 @@ class SupplierRequestController extends Controller
         }
     }
 
+    function getStateId($stateName)
+    {
+        return DB::table('state_requests')->where('name', $stateName)->value('id');
+    }
 
     /**
      * Show the form for creating a new resource.
