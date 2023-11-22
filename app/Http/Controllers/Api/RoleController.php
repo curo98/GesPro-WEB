faa4689f1f172af5d5570cec4789f461c20b4301
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-22 13:03:08 -0500

    Update rol

--- app/Http/Controllers/Api/RoleController.php
+++ app/Http/Controllers/Api/RoleController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Models\Role;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RoleController extends Controller
 {
