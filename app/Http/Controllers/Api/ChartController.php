0bda1ffc662c5ace203330143a924ce23bfef7b1
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-23 20:41:16 -0500

    Chart bart.

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Models\User;
 use App\Models\SupplierRequest;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ChartController extends Controller
 {
