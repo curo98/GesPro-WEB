cf9c0a9a01d7dbeef425d683a6abf3f2f0d3e09f
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-30 12:26:34 -0500

    Modify DatabaseSeeder....

--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -16,28 +16,28 @@ class SupplierFactory extends Factory
      */
     public function definition(): array
     {
-        // Lista de países latinoamericanos
-        $latinAmericanCountries = ['Perú', 'México', 'Argentina', 'Brasil', 'Colombia', 'Chile', 'Ecuador', 'Venezuela', 'Bolivia', 'Uruguay'];
+        // // Lista de países latinoamericanos
+        // $latinAmericanCountries = ['Perú', 'México', 'Argentina', 'Brasil', 'Colombia', 'Chile', 'Ecuador', 'Venezuela', 'Bolivia', 'Uruguay'];
 
-        // Obtener un usuario con rol "invitado"
-        $invitadoUser = \App\Models\User::where('name', 'invitado')->first();
+        // // Obtener un usuario con rol "invitado"
+        // $invitadoUser = \App\Models\User::where('name', 'invitado')->first();
 
-        if (!$invitadoUser) {
-            $invitadoUser = \App\Models\User::factory()->create([
-                'name' => 'invitado',
-                'email' => 'invitado@example.com',
-                'id_role' => 1, // Asigna el ID del rol correspondiente
-            ]);
-        }
+        // if (!$invitadoUser) {
+        //     $invitadoUser = \App\Models\User::factory()->create([
+        //         'name' => 'invitado',
+        //         'email' => 'invitado@example.com',
+        //         'id_role' => 1, // Asigna el ID del rol correspondiente
+        //     ]);
+        // }
 
-        // Genera un país aleatorio: Perú o un país latinoamericano con mayor probabilidad
-        $country = $this->faker->randomElement(array_merge([$this->faker->randomElement($latinAmericanCountries)], $latinAmericanCountries));
+        // // Genera un país aleatorio: Perú o un país latinoamericano con mayor probabilidad
+        // $country = $this->faker->randomElement(array_merge([$this->faker->randomElement($latinAmericanCountries)], $latinAmericanCountries));
 
-        return [
-            'nacionality' => $country,
-            'nic_ruc' => $this->faker->unique()->numerify('##########'), // Ejemplo de número aleatorio de 10 dígitos
-            'id_user' => $invitadoUser->id,
-            'state' => 'Inactivo',
-        ];
+        // return [
+        //     'nacionality' => $country,
+        //     'nic_ruc' => $this->faker->unique()->numerify('##########'), // Ejemplo de número aleatorio de 10 dígitos
+        //     'id_user' => $invitadoUser->id,
+        //     'state' => 'Inactivo',
+        // ];
     }
 }
