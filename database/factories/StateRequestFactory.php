4bcdf300d99703d7dd5c11c0dce82d665f37ac34
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-11-30 11:45:21 -0500

    Modify DatabaseSeeder.

--- database/factories/StateRequestFactory.php
+++ database/factories/StateRequestFactory.php
@@ -18,7 +18,35 @@ class StateRequestFactory extends Factory
     {
         return [
             'name' => $this->faker->unique()->randomElement(['Enviado', 'Por recibir', 'Recibida', 'Por validar', 'Validada', 'Por aprobar', 'Aprobada', 'Desaprobada', 'Cancelada']),
-            'description' => $this->faker->sentence(),
+            'description' => $this->generateDescription(),
         ];
+    }
+
+    private function generateDescription(): string
+    {
+        $status = $this->faker->randomElement(['Enviado', 'Por recibir', 'Recibida', 'Por validar', 'Validada', 'Por aprobar', 'Aprobada', 'Desaprobada', 'Cancelada']);
+
+        switch ($status) {
+            case 'Enviado':
+                return 'La solicitud ha sido enviada y está pendiente de revisión.';
+            case 'Por recibir':
+                return 'La solicitud está en camino y se espera su recepción.';
+            case 'Recibida':
+                return 'La solicitud ha sido recibida correctamente.';
+            case 'Por validar':
+                return 'La solicitud está pendiente de validación.';
+            case 'Validada':
+                return 'La solicitud ha sido validada.';
+            case 'Por aprobar':
+                return 'La solicitud está pendiente de aprobación.';
+            case 'Aprobada':
+                return 'La solicitud ha sido aprobada y procesada.';
+            case 'Desaprobada':
+                return 'La solicitud ha sido desaprobada y no será procesada.';
+            case 'Cancelada':
+                return 'La solicitud ha sido cancelada antes de su procesamiento.';
+            default:
+                return 'Descripción no especificada para este estado.';
+        }
     }
 }
