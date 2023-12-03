8d8cf94b2f9f03ba26e396152f5f992aff784def
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-12-03 14:46:03 -0500

    add photo profile...

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,6 +8,8 @@ use \App\Models\User;
 use \App\Models\Role;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 use Illuminate\Support\Facades\Auth;
 
