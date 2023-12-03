aa91349abc8a0fc1831eb448025e0ec0556c954f
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-12-03 10:47:34 -0500

    flags.

--- database/migrations/2023_10_23_210639_create_countries_table.php
+++ database/migrations/2023_10_23_210639_create_countries_table.php
@@ -14,6 +14,14 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('iso_alpha2');
+            $table->string('iso_alpha3');
+            $table->unsignedInteger('iso_numeric'); // Change to unsigned integer
+            $table->string('currency_code');
+            $table->string('currency_name');
+            $table->string('currency_symbol');
+            $table->string('flag'); // Assuming 'flag' is a file path or URL
+            $table->timestamps();
         });
     }
 
