168c636bcad38b959cfd84085794d2f3a4b99d62
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-12-09 11:33:34 -0500

    Init project ux..

--- database/migrations/2023_12_09_104511_create_activities_table.php
+++ database/migrations/2023_12_09_104511_create_activities_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->TEXT('description');
             $table->timestamps();
         });
     }
